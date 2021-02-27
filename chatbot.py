80355b3926877836d5c388546438c6cf67cfc260
Prostigus <prajeetdidden@gmail.com>
2021-02-26 18:19:51 -0800

    DeepLearning using tensorflow

--- chatbot.py
+++ chatbot.py
@@ -1,51 +1,33 @@
-import nltk
-from nltk.chat.util import Chat, reflections
-print("Hello! This is The COSC310Chatbot. Feel free to ask any questions. To quit say a \"goodbye\" phrase")
+from load import load
+import numpy
+import random
 
-set_pairs = [
-    [
-        r"my name is (.*)",
-        ["Hello %1, How are you doing today?",]
-    ],
-    [
-        r"hi|hey|hello",
-        ["Hello!", "Hey there!",]
-    ], 
-    [
-        r"what is your name",
-        ["You can call me The COSC310Chatbot.",]
-    ],
-    [
-        r"how are you|how are you doing|how are you feeling",
-        ["I am fine, thank you! How can I help you?",]
-    ],
-    [
-        r"i am fine, thank you|i am fine|im fine|i'm fine|i am good, thank you|i am good|im good|i'm good",
-        ["Great to hear that, how can I help you?",]
-    ],
-    [
-        r"how can i help you",
-        ["I am suppose to be helping you. So how can I help you",]
-    ],
-    [
-        r"i'm doing good|i'm (.*) doing good",
-        ["That's great to hear",]
-    ],
-    [
-        r"(.*) thank you so much, that was helpful",
-        ["Iam happy to help", "No problem, you're welcome",]
-    ],
-    [
-        r"goodbye|bye|take care|end|quit",
-        ["Bye, take care. See you soon :) ","It was nice talking to you. See you soon :)"]
-],
-]
+l = load("intents.json")
+data = l.getData()
+l.Process()
+model = l.getModel()
+words = l.getWords()
+labels = l.getLabels()
 
-def chatbot():
-        print("") 
 
-chat = Chat(set_pairs, reflections)
+def chat():
+    print("Start talking with the bot (type quit to stop)!")
+    while True:
+        inp = input("You: ")
+        if inp.lower() == "quit":
+            break
 
-chat.converse()
-if __name__ == "__main__":
-    chatbot()
+        results = model.predict([l.bag_of_words(inp, words)])[0]
+        results_index = numpy.argmax(results)
+        tag = labels[results_index]
+        print(results)
+        if results[results_index] > 0.8:
+            for t in data["intents"]:
+                if t['tag'] == tag:
+                    responses = t['responses']
+
+            print(random.choice(responses))
+        else:
+            print("I didn't quite understand")
+
+chat()
---
+++ load.py
@@ -0,0 +1,120 @@
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy
+import tflearn
+import tensorflow
+import random
+import json
+import pickle
+
+class load:
+    def __init__(self,f):
+        with open(f) as file:
+            data = json.load(file)
+        self.data = data
+    
+
+    def Process(self):
+        try:
+            with open("data.pickle", "rb") as f:
+                words, labels, training, output = pickle.load(f)
+        except:
+            words =  []
+            labels = []
+            docs_x = []
+            docs_y = []
+            data = self.data
+
+            for intent in data["intents"]:
+                for pattern in intent["patterns"]:
+                    wrds = nltk.word_tokenize(pattern)
+                    words.extend(wrds)
+                    docs_x.append(wrds)
+                    docs_y.append(intent["tag"])
+
+                if intent["tag"] not in labels:
+                    labels.append(intent["tag"])
+
+            words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+            words = sorted(list(set(words)))
+
+            labels = sorted(labels)
+
+            training = []
+            output = []
+
+            out_empty = [0 for _ in range(len(labels))]
+
+            for x, doc in enumerate(docs_x):
+                bag = []
+
+                wrds = [stemmer.stem(w.lower()) for w in doc]
+
+                for w in words:
+                    if w in wrds:
+                        bag.append(1)
+                    else:
+                        bag.append(0)
+
+                output_row = out_empty[:]
+                output_row[labels.index(docs_y[x])] = 1
+
+                training.append(bag)
+                output.append(output_row)
+
+
+            training = numpy.array(training)
+            output = numpy.array(output)
+
+            with open("data.pickle", "wb") as f:
+                pickle.dump((words, labels, training, output), f)
+
+        tensorflow.compat.v1.reset_default_graph()
+
+        net = tflearn.input_data(shape=[None, len(training[0])])
+        net = tflearn.fully_connected(net, 8)
+        net = tflearn.fully_connected(net, 8)
+        net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+        net = tflearn.regression(net)
+
+        model = tflearn.DNN(net)
+        
+        try:
+            model.load("model.tflearn")
+        except:
+            model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+            model.save("model.tflearn")
+        self.words = words
+        self.model = model
+        self.labels = labels
+
+
+
+    def bag_of_words(self,s,words):
+        bag = [0 for _ in range(len(words))]
+
+        s_words = nltk.word_tokenize(s)
+        s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+        for se in s_words:
+            for i, w in enumerate(words):
+                if w == se:
+                    bag[i] = 1
+            
+        return numpy.array(bag)
+
+    def getData(self):
+        return self.data
+
+    def getModel(self):
+        return self.model
+
+    def getWords(self):
+        return self.words
+    
+    def getLabels(self):
+        return self.labels
+
+   

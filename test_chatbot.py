39db3b340e515224ec8adafe1d6503532e7af2a2
Prostigus <prajeetdidden@gmail.com>
2021-03-26 17:43:24 -0700

    Completed test cases Fixes #4

--- test_chatbot.py
+++ test_chatbot.py
@@ -20,10 +20,19 @@ class TestChatbot(unittest.TestCase):
 
     def test_chat(self):                
         self.assertTrue(chatbot.chat("How are you") in self.responses)
+        self.assertTrue(chatbot.chat("Is anyone there?") in self.responses)
+        self.assertTrue(chatbot.chat("hello") in self.responses)
+        self.assertTrue(chatbot.chat("good day") in self.responses)
+        self.assertTrue(chatbot.chat("whats up?") in self.responses)
+               
     
     def test_tag(self):
         chatbot.chat("How are you")
         self.assertEqual(chatbot.tag,"greeting")
+        chatbot.chat("see you later")
+        self.assertEqual(chatbot.tag,"goodbye")
+        chatbot.chat("how old")
+        self.assertEqual(chatbot.tag,"age")
 
     def test_sentiment(self):
         self.assertEqual(chatbot.chat("I love your work"),"Glad to hear you really like that.")
@@ -43,8 +52,11 @@ class TestChatbot(unittest.TestCase):
     def test_synonyms(self):
         self.assertTrue(chatbot.chat("what type of music do you listen to") in self.responsesmusic)
         self.assertTrue(chatbot.chat("what tunes do you listen to") in self.responsesmusic)
-
         
+
+    def test_failResponses(self):
+        self.assertTrue(chatbot.chat("Who is Gahlran?") in chatbot.others)
+        self.assertTrue(chatbot.chat("do you play among us?") in chatbot.others)
 
 
 if __name__ == '__main__':
